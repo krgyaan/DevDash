5942c2885909b6955193f6ef7e35099ffd71dd18
krgyaan <gyanprakashk55@gmail.com>
2024-01-03 00:03:21 +0530

    deployment ready

---
+++ frontend/src/pages/CodeField/Codespace.js
@@ -0,0 +1,60 @@
+const codespace = [
+    {
+        id: 1,
+        title: 'Web Development',
+        description: 'Web development can range from developing a simple single static page of plain text to complex Web-based Internet applications, electronic businesses, and social network services.',
+        availavlity: 'Free',
+        image: 'https://img.icons8.com/dusk/512/google-code.png',
+        link: 'https://www.w3schools.com/html/default.asp'
+    },
+    {
+        id: 2,
+        title: 'Java',
+        description: 'Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.',
+        availavlity: 'Not Free',
+        image: 'https://img.icons8.com/plasticine/400/java-coffee-cup-logo.png',
+        link: 'https://www.w3schools.com/java/default.asp'
+    },
+    {
+        id: 3,
+        title: 'Python',
+        description: 'Python is an interpreted, high-level and general-purpose programming language. Python\'s design philosophy emphasizes code readability with its notable use of significant indentation.',
+        availavlity: 'Free',
+        image: 'https://img.icons8.com/clouds/500/python.png',
+        link: 'https://www.w3schools.com/python/default.asp'
+    },
+    {
+        id: 4,
+        title: 'C++',
+        description: 'C++ is a general-purpose programming language created by Bjarne Stroustrup as an extension of the C programming language, or "C with Classes".',
+        availavlity: 'Not Free',
+        image: 'https://img.icons8.com/color/240/c-plus-plus-logo.png',
+        link: 'https://www.w3schools.com/cpp/default.asp'
+    },
+    {
+        id: 5,
+        title: 'C',
+        description: 'C is a general-purpose, procedural computer programming language supporting structured programming, lexical variable scope, and recursion, with a static type system.',
+        availavlity: 'Free',
+        image: 'https://img.icons8.com/fluency/240/c-programming.png',
+        link: 'https://www.w3schools.com/cpp/default.asp'
+    },
+    {
+        id: 6,
+        title: 'PHP',
+        description: 'PHP is a general-purpose scripting language especially suited to web development. It was originally created by Danish-Canadian programmer Rasmus Lerdorf in 1994.',
+        availavlity: 'Not Free',
+        image: 'https://img.icons8.com/officel/480/php-logo.png',
+        link: 'https://www.w3schools.com/php/default.asp'
+    },
+    {
+        id: 7,
+        title: 'Request New CodeField',
+        description: 'Request a new code to be added to the CodeFields.',
+        availavlity: 'Free',
+        image: 'https://img.icons8.com/external-flaticons-lineal-color-flat-icons/64/external-request-advertising-agency-flaticons-lineal-color-flat-icons.png',
+        link: 'https://www.w3schools.com/php/default.asp'
+    }
+];
+
+export default codespace;
